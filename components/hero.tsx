d26a2071d0ca7a2f1335a98bbc36ed86b3635227
Carlos <bisolaolamide178@gmail.com>
2024-02-06 16:25:02 +0500

    Powered by

--- components/hero.tsx
+++ components/hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">Bentel LLC</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Book Yourself an Appointment with our Professional.</p>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Powered by MARKOR</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <Link href="/signup" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 ml-3">
